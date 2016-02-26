038d7913b77b678379c51ba284900b9cb6f451e4
Rohit Gupta <rohit221991@iitj.ac.in>
2016-02-26 16:43:08 +0530

    working phase 1

--- src/main/java/utils/ResponseUtil.java
+++ src/main/java/utils/ResponseUtil.java
@@ -5,50 +5,7 @@ package utils;
  */
 public class ResponseUtil {
 
-   static String json ="{\n" +
-           "\t\"total\": 1,\n" +
-           "\t\"count\": 1,\n" +
-           "\t\"items\": [{\n" +
-           "\t\t\"total\": 1,\n" +
-           "\t\t\"likedTimestamp\": 0,\n" +
-           "\t\t\"localizedTitles\": {},\n" +
-           "\t\t\"count\": 15,\n" +
-           "\t\t\"exclusive\": false,\n" +
-           "\t\t\"downloaded\": 0,\n" +
-           "\t\t\"type\": \"PACKAGE\",\n" +
-           "\t\t\"id\": \"srch_bsb_1404393019358\",\n" +
-           "\t\t\"title\": \"Latest Hindi\",\n" +
-           "\t\t\"originalImage\": \"http:\\/\\/s3-ap-southeast-1.amazonaws.com\\/bsbcms\\/music\\/package\\/new-releases1-generic.png\",\n" +
-           "\t\t\"mediumImage\": \"http:\\/\\/img.wynk.in\\/unsafe\\/240x240\\/top\\/http:\\/\\/d2n2xdxvkri1jk.cloudfront.net\\/music\\/package\\/new-releases1-generic.png\",\n" +
-           "\t\t\"thumbnailUrl\": \"http:\\/\\/img.wynk.in\\/unsafe\\/50x50\\/top\\/http:\\/\\/d2n2xdxvkri1jk.cloudfront.net\\/music\\/package\\/new-releases1-generic.png\",\n" +
-           "\t\t\"shortUrl\": \"http:\\/\\/wynk.in\\/u\\/107koXui4QFUzG\",\n" +
-           "\t\t\"smallImage\": \"http:\\/\\/img.wynk.in\\/unsafe\\/120x120\\/top\\/http:\\/\\/d2n2xdxvkri1jk.cloudfront.net\\/music\\/package\\/new-releases1-generic.png\",\n" +
-           "\t\t\"rented\": 0,\n" +
-           "\t\t\"offset\": 0,\n" +
-           "\t\t\"rentedTimestamp\": 0,\n" +
-           "\t\t\"musicPackageType\": \"NEW_RELEASES\",\n" +
-           "\t\t\"liked\": false,\n" +
-           "\t\t\"largeImage\": \"http:\\/\\/img.wynk.in\\/unsafe\\/320x180\\/top\\/http:\\/\\/d2n2xdxvkri1jk.cloudfront.net\\/music\\/package\\/new-releases1-generic.png\",\n" +
-           "\t\t\"lastUpdated\": 1456476546939,\n" +
-           "\t\t\"deleted\": false,\n" +
-           "\t\t\"downloadedTimestamp\": 0,\n" +
-           "\t\t\"contentToProviderCount\": {\n" +
-           "\t\t\t\"EROS\": 2,\n" +
-           "\t\t\t\"HUNGAMA\": 29,\n" +
-           "\t\t\t\"UNIVERSAL\": 2,\n" +
-           "\t\t\t\"PPL_MUMBAI\": 1,\n" +
-           "\t\t\t\"SAREGAMA\": 5,\n" +
-           "\t\t\t\"SONY\": 4,\n" +
-           "\t\t\t\"UNISYS\": 10,\n" +
-           "\t\t\t\"OTHERS\": 4\n" +
-           "\t\t},\n" +
-           "\t\t\"subtype\": \"New Releases\",\n" +
-           "\t\t\"itemTypes\": [\"SONG\"]\n" +
-           "\t}],\n" +
-           "\t\"offset\": 0,\n" +
-           "\t\"lang\": \"en\"\n" +
-           "}";
-
+   static String json ="{\"total\":1,\"count\":1,\"items\":[{\"total\":1,\"likedTimestamp\":0,\"localizedTitles\":{},\"count\":15,\"exclusive\":false,\"downloaded\":0,\"type\":\"PACKAGE\",\"id\":\"srch_bsb_1404393019358\",\"title\":\"Latest Hindi\",\"originalImage\":\"http:\\/\\/s3-ap-southeast-1.amazonaws.com\\/bsbcms\\/music\\/package\\/new-releases1-generic.png\",\"mediumImage\":\"http:\\/\\/img.wynk.in\\/unsafe\\/240x240\\/top\\/http:\\/\\/d2n2xdxvkri1jk.cloudfront.net\\/music\\/package\\/new-releases1-generic.png\",\"thumbnailUrl\":\"http:\\/\\/img.wynk.in\\/unsafe\\/50x50\\/top\\/http:\\/\\/d2n2xdxvkri1jk.cloudfront.net\\/music\\/package\\/new-releases1-generic.png\",\"shortUrl\":\"http:\\/\\/wynk.in\\/u\\/107koXui4QFUzG\",\"smallImage\":\"http:\\/\\/img.wynk.in\\/unsafe\\/120x120\\/top\\/http:\\/\\/d2n2xdxvkri1jk.cloudfront.net\\/music\\/package\\/new-releases1-generic.png\",\"rented\":0,\"offset\":0,\"rentedTimestamp\":0,\"musicPackageType\":\"NEW_RELEASES\",\"liked\":false,\"largeImage\":\"http:\\/\\/img.wynk.in\\/unsafe\\/320x180\\/top\\/http:\\/\\/d2n2xdxvkri1jk.cloudfront.net\\/music\\/package\\/new-releases1-generic.png\",\"lastUpdated\":1456476546939,\"deleted\":false,\"downloadedTimestamp\":0,\"contentToProviderCount\":{\"EROS\":2,\"HUNGAMA\":29,\"UNIVERSAL\":2,\"PPL_MUMBAI\":1,\"SAREGAMA\":5,\"SONY\":4,\"UNISYS\":10,\"OTHERS\":4},\"items\":[{\"genre\":{\"id\":\"1001\",\"title\":\"Dance\"},\"likedTimestamp\":0,\"contentLang\":\"hi\",\"exclusive\":false,\"album\":\"Kapoor & Sons\",\"purchaseUrl\":\"http:\\/\\/api.wynk.in\\/music\\/v1\\/cdcurl\\/song\\/srch_sonymusic_INS171600163.html\",\"downloadUrl\":\"http:\\/\\/api.wynk.in\\/music\\/v1\\/cdcgw\\/song\\/srch_sonymusic_INS171600163.html\",\"downloaded\":0,\"hellotunes\":[{\"title\":\"Kar Gayi Chull\",\"price\":\"15\",\"purchaseUrl\":\"http:\\/\\/125.21.246.72\\/v1\\/operator\\/ht\\/provision?vcode=009153200004941&imgUrl=http%3A%2F%2Fimg.wynk.in%2Funsafe%2F240x240%2Ftop%2Fhttp%3A%2F%2Fd2n2xdxvkri1jk.cloudfront.net%2Fmusic%2F1455862145%2Fsrch_sonymusic_INS171600163.jpg\",\"operator\":null,\"previewUrl\":\"http:\\/\\/www.airtelhellotunes.in\\/tone\\/009153200004941.wav\",\"vcode\":\"009153200004941\"}],\"type\":\"SONG\",\"formats\":[\"hd\",\"h\",\"m\",\"l\"],\"publisher\":\"Sony Music Entertainment India Pvt. Ltd.\",\"id\":\"srch_sonymusic_INS171600163\",\"cpId\":\"srch_sonymusic\",\"title\":\"Kar Gayi Chull\",\"rentUrl\":\"http:\\/\\/api.wynk.in\\/music\\/v1\\/crcgw\\/srch_sonymusic_INS171600163.html\",\"originalImage\":\"http:\\/\\/s3-ap-southeast-1.amazonaws.com\\/bsbcms\\/music\\/1455862145\\/srch_sonymusic_INS171600163.jpg\",\"mediumImage\":\"http:\\/\\/img.wynk.in\\/unsafe\\/240x240\\/top\\/http:\\/\\/d2n2xdxvkri1jk.cloudfront.net\\/music\\/1455862145\\/srch_sonymusic_INS171600163.jpg\",\"thumbnailUrl\":\"http:\\/\\/img.wynk.in\\/unsafe\\/50x50\\/top\\/http:\\/\\/d2n2xdxvkri1jk.cloudfront.net\\/music\\/1455862145\\/srch_sonymusic_INS171600163.jpg\",\"shortUrl\":\"http:\\/\\/wynk.in\\/u\\/101iLmqo0qFTjR\",\"rented\":0,\"smallImage\":\"http:\\/\\/img.wynk.in\\/unsafe\\/120x120\\/top\\/http:\\/\\/d2n2xdxvkri1jk.cloudfront.net\\/music\\/1455862145\\/srch_sonymusic_INS171600163.jpg\",\"ostreamingUrl\":\"http:\\/\\/www.mymp3song.com\\/files\\/download\\/id\\/37678\",\"rentedTimestamp\":0,\"downloadPrice\":\"10\",\"musicPackageType\":\"UNKNOWN\",\"keywords\":\"Kapoor & Sons,Alia Bhatt, Sukriti Kakar,Sukriti Kakar, Badshah,Kumaar,Badshah,Kar Gayi Chull,Rishi Kapoor,Neha Kakkar,Sidharth Malhotra, Fazilpuria,Amaal Mallik,Fawad Khan,Fazilpuria\",\"videos\":[],\"liked\":false,\"subtitleType\":\"ALBUM\",\"largeImage\":\"http:\\/\\/img.wynk.in\\/unsafe\\/320x180\\/top\\/http:\\/\\/d2n2xdxvkri1jk.cloudfront.net\\/music\\/1455862145\\/srch_sonymusic_INS171600163.jpg\",\"mood\":{\"id\":\"41601114513153151234123111651528616151111316\",\"title\":\"Relaxed\"},\"label\":\"Sony Music Entertainment India Pvt. Ltd.\",\"deleted\":false,\"publishedYear\":\"2016\",\"radioEnabled\":false,\"subtitleId\":\"srch_sonymusic_886445735352\",\"downloadedTimestamp\":0,\"hlsUrlAdaptiveWithoutHD\":\"http:\\/\\/cms.twangmusic.in\\/srch_sonymusic\\/music\\/,128,64,32,\\/1455862146\\/srch_sonymusic_INS171600163.mp4.csmil\\/master.m3u8\",\"duration\":187,\"subtitle\":\"Neha Kakkar-Kapoor & Sons\",\"albumRef\":{\"id\":\"srch_sonymusic_886445735352\",\"title\":\"Kapoor & Sons\"},\"oStreamingQuality\":\"h\",\"streamingPrice\":\"7.0\"}],\"subtype\":\"New Releases\",\"itemTypes\":[\"SONG\"]}] ,\"offset\":0,\"lang\":\"en\"}";
     public static String getResponse(){return json;}
 
 }
